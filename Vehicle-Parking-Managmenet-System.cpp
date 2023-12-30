9d73488ae4bdaa011209f010347cf13145c6d9e1
Mohammad Reduwan <ridwan0gts@gmail.com>
2023-12-30 17:50:12 +0800

    Edited the user interface

--- Vehicle-Parking-Managmenet-System.cpp
+++ Vehicle-Parking-Managmenet-System.cpp
@@ -6,18 +6,22 @@ int main() {
 	int user_input;
 	int amount = 0;
 	int count = 0;
+	int choice = 0;
+	bool flag = true;
 
-	while (true) {
+	cout << "\n";
+	cout << "VEHICLE-----PARKING----MANAGEMENT----SYSTEM" << endl;
+	cout << "\n";
+
+	while (flag) {
 		
-		cout << "\n";
-		cout << "VEHICLE-----PARKING----MANAGEMENT----SYSTEM" << endl;
-
 		cout << "\n";
 		cout << "PRESS 1: For Motorcycles" << endl;
 		cout << "PRESS 2: For Car" << endl;
 		cout << "PRESS 3: For Bus" << endl;
 		cout << "PRESS 4: To Show Record" << endl;
 		cout << "PRESS 5: To Delete Record" << endl;
+		cout << "PRESS 6: To EXIT" << endl;
 		cout << "Enter your choice: ";
 		cin >> user_input;
 		if (user_input == 1) {
@@ -41,10 +45,12 @@ int main() {
 			amount = 0;
 			count = 0;
 		}
+		else if(user_input == 6){
+			flag = false;
+		}
 		else {
 			cout << "ENTER THE VALID NUMBER WHICH CAN BE MAPPED WITH THE ABOVE OPERATIONS!" << endl;
 		}
-		
 	}
 
 	return 0;
