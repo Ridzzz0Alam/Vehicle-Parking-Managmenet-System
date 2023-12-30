19b2b62c5bf909176745001aec6dcc6000636ff9
Mohammad Reduwan <ridwan0gts@gmail.com>
2023-12-30 17:33:49 +0800

    Created main menu user interface

--- Vehicle-Parking-Managmenet-System.cpp
+++ Vehicle-Parking-Managmenet-System.cpp
@@ -8,7 +8,18 @@ int main() {
 	int count = 0;
 
 	while (true) {
+		
+		cout << "\n";
+		cout << "VEHICLE-----PARKING----MANAGEMENT----SYSTEM" << endl;
 
-
+		cout << "\n";
+		cout << "PRESS 1: For Motorcycles" << endl;
+		cout << "PRESS 2: For Car" << endl;
+		cout << "PRESS 3: For Bus" << endl;
+		cout << "PRESS 4: To Show Record" << endl;
+		cout << "PRESS 5: To Delete Record" << endl;
+		cout << "Enter your choice: ";
+		cin >> user_input;
+		
 	}
 }
